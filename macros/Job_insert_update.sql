2d71b7454c9fe95349c9987631c0a7b1d9327b86
basak.asanko@gmail.com <basak.asanko@gmail.com>
2022-02-02 14:37:23 +0000

    ABC version 2 feb

--- macros/Job_insert_update.sql
+++ macros/Job_insert_update.sql
@@ -1,9 +1,9 @@
-{% macro Job_insert_update(running_job_status,running_job_name,running_job_id) %}
+{% macro Job_insert_update(running_job_status,running_job_name,running_job_id,b_id) %}
 
     {% if running_job_status == 'INSERT' %}
         {% set query -%}
-            Insert into ABC.PUBLIC.ABC_Job_Details(Job_ID,JobName,Start_Timestamp,Job_Status) 
-            VALUES ('{{running_job_id}}','{{running_job_name}}',sysdate(),'In_Progress')
+            Insert into ABC.PUBLIC.ABC_Job_Details(Job_ID,JobName,Start_Timestamp,Job_Status,BATCH_ID) 
+            VALUES ('{{running_job_id}}','{{running_job_name}}',sysdate(),'In_Progress','{{b_id}}')
         {%- endset %}
 
         {% do run_query(query) %}
@@ -21,7 +21,7 @@
 
         {% set query -%}
             Update ABC.PUBLIC.ABC_Job_Details
-	        SET End_Timestamp = sysdate(),Job_Status = 'SUCCESS' where where Job_ID = '{{running_job_id}}'   ;
+	        SET End_Timestamp = sysdate(),Job_Status = 'SUCCESS' where Job_ID = '{{running_job_id}}'   ;
         {%- endset %}
 
         {% do run_query(query) %}
