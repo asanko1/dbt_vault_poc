78976d24ca6e3575e22dd5975058f910a5938d8f
basak.asanko@gmail.com <basak.asanko@gmail.com>
2022-02-04 02:06:28 +0000

    Testing of Split works

--- macros/Testing_This.sql
+++ macros/Testing_This.sql
@@ -1,4 +1,6 @@
 {% macro testthis() %}
+    {%- set current_model_full_name = '{{ this }}'.split('.') -%}
+    {%- set current_table_name =current_model_full_name[-1] -%}
     insert into ABC.Public.TestThis (ColName)
-    Select '{{this}}'
+    Select '{{current_table_name}}'
 {% endmacro %}
