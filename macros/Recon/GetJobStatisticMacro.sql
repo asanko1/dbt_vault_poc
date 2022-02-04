0c410b671d1120ccba07e9f9a9ab2772b8911d98
basak.asanko@gmail.com <basak.asanko@gmail.com>
2022-02-04 01:07:37 +0000

    Added Statistics Macro

--- macros/Recon/GetJobStatisticMacro.sql
+++ macros/Recon/GetJobStatisticMacro.sql
@@ -1,16 +1,31 @@
-{% macro get_query_result() %}    
+{% macro GetJobStatisticMacro() %}
     {%- call statement('GetMetrics', fetch_result=True) -%}
         Select SourceTableName,    SourceAttributeNames,
         OperationName,JobMetricsMaster_id from ABC.Public.JobMetricsMaster
-        WHERE SourceTableName = 'RAW_ORDERS' 
+        WHERE SourceTableName = '{{this}}' 
     {%- endcall -%}
-    {%- set getmetrics= load_result('GetMetrics') -%}
-    {%- set count1= load_result('GetMetrics')['response']['rows_affected'] -%}
-    {%- set check1= load_result('GetMetrics')['data'][0][3] -%}
-    {{return (getmetrics)}}
-    
-{% endmacro %}
+    {% if execute %}
+        {%- set get_metrics_frame= load_result('GetMetrics') -%}
+        {%- set no_of_metrics= get_metrics_frame['response']['rows_affected'] | int -%}
+        {%- set get_metrics_array= load_result('GetMetrics')['data'] -%}
+        {% set query_metric_entries %}
+            {% for i in range(no_of_metrics) %}
+                Insert into ABC.Public.ABC_JOBBALANCE(JOB_ID,Batch_ID,Metric_ID,SourceValue)
+                Select  concat('{{running_job_id}}','-','{{this}}'),'{{batch_id}}',count(distinct {{get_metrics_array[i][1]}}) , {{get_metrics_array[i][3]}}
+                from ShreyDBTPOC.Demo_dev.{{get_metrics_array[i][0]}}
+            {% endfor %}
+        {% endset %}
 
-print {{getmetrics}}
+    {% endif %}
+    {{query_metric_entries}}
+{% endmacro %}
+   
+
+
+  
+
+    
+
+
 
    
