18b83c6bf5abe6d162cce5393c2c729d3caf1554
talktorishin94@gmail.com <talktorishin94@gmail.com>
2022-02-07 10:24:18 +0000

    Added the code for abc framework

--- macros/run_end_hook.sql
+++ macros/run_end_hook.sql
@@ -1,17 +1,25 @@
-{% macro run_end_hook(job_id,table_name) %}
+{% macro run_end_hook(job_id,table_name,job_name) %}
 
-    {% if execute %}
+    
         {%- call statement('delta_count', fetch_result=True) -%}
-            SELECT count(*) FROM {{table_name}} where JOBID = concat( '{{job_id}}','-','inventory')
+            SELECT count(*) FROM {{table_name}} where JOBID = to_varchar('{{job_id}}')
         {%- endcall -%}
+        {% set query -%}
+            Insert into PC_DBT_DB.DBT_ABASAK_CUST_DETAIL.job_id(Job_ID) 
+            VALUES ('{{job_id}}') ;
+        {%- endset %}
 
-        {%- set Query_count = load_result('delta_count')  -%}
-        {%- set out_result = Query_count['data'][0][0] -%}
-
-        {% if out_result > 0 %}
-            {{ Job_insert_update('SUCCESS','{{table_name}}','pipeline_id','batch_id','inventory') }}
-        {% else %}
-            {{ Job_insert_update('FAIL','{{table_name}}','pipeline_id','batch_id','inventory') }}
+        {% do run_query(query) %}
+        
+        {% if execute %}    
+            {%- set Query_count = load_result('delta_count')['data'][0][0] | int -%}
+            {%- set out_result = Query_count | int -%}
+           
+            
+            {% if out_result > 0 %} 
+                {{ Job_insert_update('SUCCESS','{{job_name}}',job_id,'batch_id') }}
+            {% else %}
+                {{ Job_insert_update('FAIL','{{job_name}}',job_id,'batch_id') }}
+            {% endif %}
         {% endif %}
-    {% endif %}
 {% endmacro %}
