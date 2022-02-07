a2980245f98f1795ca210c6bac80561c2ad74247
talktorishin94@gmail.com <talktorishin94@gmail.com>
2022-02-07 21:33:41 +0000

    removed "testthis" macro

--- macros/Testing_This.sql
+++
@@ -1,8 +0,0 @@
-{% macro testthis() %}
-    {%- set current_model_full_name = '{{ this }}'.split('.') -%}
-    {% if execute %}
-        {%- set current_table_name =current_model_full_name[-1] -%}
-        insert into ABC.Public.TestThis (ColName)
-        Select '{{current_table_name}}'
-    {% endif %}
-{% endmacro %}
