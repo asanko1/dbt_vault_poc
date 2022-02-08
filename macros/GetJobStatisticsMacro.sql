343dd8a03dd71df4741c80a601eea602294e93dd
talktorishin94@gmail.com <talktorishin94@gmail.com>
2022-02-08 00:39:59 +0000

    updated stats

--- macros/GetJobStatisticsMacro.sql
+++ macros/GetJobStatisticsMacro.sql
@@ -10,13 +10,13 @@
         {%- set get_metrics_frame= load_result('GetMetrics') -%}
         {%- set no_of_metrics= get_metrics_frame['response']['rows_affected'] | int -%}
         {%- set get_metrics_array= load_result('GetMetrics')['data'] -%}
-        {% set query_metric_entries %}
+        {%- set query_metric_entries -%}
             {% for i in range(no_of_metrics) %}
                 Insert into ABC.Public.ABC_JOBBALANCE(JOB_ID,Batch_ID,Metric_ID,SourceValue)
                 Select '{{model_id}}','{{batch_id}}',count(distinct {{get_metrics_array[i][1]}}) , {{get_metrics_array[i][3]}}
-                from ShreyDBTPOC.Demo_dev.{{get_metrics_array[i][0]}}
+                from ShreyDBTPOC.Demo_dev.{{get_metrics_array[i][0]}};
             {% endfor %}
-        {% endset %}
+        {%- endset -%}
     {% endif %}
-    {{query_metric_entries}}
+     {% do run_query(query_metric_entries) %}
 {% endmacro %}
