a21359308f4de6c3efc89b2a1881cefc7cb5cc4d
talktorishin94@gmail.com <talktorishin94@gmail.com>
2022-02-09 14:10:42 +0000

    commiting the restartablity working code

--- macros/Common_DQ_Checks.sql
+++ macros/Common_DQ_Checks.sql
@@ -1,4 +1,4 @@
-{% test string_length_check(model, column_name, desired_len) %}
+{% test str_len_check(model, column_name, desired_len) %}
 
     {% set name = 'len_chk__' ~ model.name ~ '__' ~ column_name %}
 
