07a6310b0f75272860f9de04110b764d315059b1
talktorishin94@gmail.com <talktorishin94@gmail.com>
2022-02-16 06:45:25 +0000

    dq_check

--- models/test_suit/sat_homsite_tests.sql
+++ models/test_suit/sat_homsite_tests.sql
@@ -4,16 +4,16 @@
         tags=["Source_system_inventory2"]
         )
 }}
-{%- set column_names = ['HOMESITEID','CITY']    -%}
+{%- set column_names = ['HOMESITEID']    -%}
 {% for column_name in column_names %}
 
-       select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,KEYVALUE AS KEYVALUE, 
-        '{{column_name}}' AS FIELDNAME, {{column_name}} as FIELDVALUE, 
+       select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE, 
+        '{{column_name}}' AS FIELDANME, {{column_name}} as FIELDVALUE, 
        '{{column_name}}'||'-DUPLICATE_VALUE' AS ISSUE , 'Error' as ErrorClassification from 
-        (select BATCH_ID,MODEL_NAME,HOMESITEID AS KEYVALUE,{{column_name}}
+        (select BATCH_ID,MODEL_NAME,{{column_name}}
             FROM {{ref('SAT_HomeSite')}}
            where {{ column_name }} is not null
-           group by BATCH_ID,MODEL_NAME,HOMESITEID,{{column_name}}
+           group by BATCH_ID,MODEL_NAME,{{column_name}}
            having count(*) > 1 ) union all 
     
 {% endfor %} 
@@ -23,7 +23,7 @@
 {% for column_name, desired_len in column_length_check.items() %}
      
        select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-        '{{column_name}}' AS FIELDNAME, {{column_name}} as FIELDVALUE,
+        '{{column_name}}' AS FIELDANME, {{column_name}} as FIELDVALUE,
        '{{column_name}}'||'-LENGTH ISSUE' AS ISSUE , 'Error' as  ErrorClassification 
            FROM {{ref('SAT_HomeSite')}}
            where {{ column_name }} is not null 
@@ -32,24 +32,24 @@
 {% endfor %}
 -- check for valid templateno 
 select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-'TEMPLATENUMBER' AS FIELDNAME, TEMPLATENUMBER as FIELDVALUE, 'INVALID Template NO','W' from {{ref('SAT_HomeSite')}}
+'TEMPLATENUMBER' AS FIELDANME, TEMPLATENUMBER as FIELDVALUE, 'INVALID Template NO','W' from {{ref('SAT_HomeSite')}}
 where ( TEMPLATENUMBER>190 and TEMPLATENUMBER<200 ) and
 TEMPLATENUMBER <> NULL  
 union all 
  SELECT BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
- 'ACTUALDELIVERYDATE' AS FIELDNAME, ACTUALDELIVERYDATE as FIELDVALUE,'DELAY_DELIVERY','W' 
+ 'ACTUALDELIVERYDATE' AS FIELDANME, ACTUALDELIVERYDATE as FIELDVALUE,'DELAY_DELIVERY','W' 
         FROM {{ref('SAT_HomeSite')}}
         WHERE ADDRESSLINE1 IS NOT NULL AND ADDRESSLINE1<>'NULL'
      AND ACTUALDELIVERYDATE > ESTIMATEDDELIVERYDATE AND SALEDATE<>'NULL' AND ACTUALDELIVERYDATE<>'NULL'
  union all 
     SELECT  BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-     'ISHOMESITECANCELLED' AS FIELDNAME, ishomesitecancelled as FIELDVALUE, 'CANCEL_STATUS_IND' ,'W' 
+     'ishomesitecancelled' AS FIELDANME, ishomesitecancelled as FIELDVALUE, 'CANCEL_STATUS_IND' ,'W' 
      FROM {{ref('SAT_HomeSite')}} 
      WHERE ishomesitecancelled='0'
      and cancellationdate <>'NULL'
  union all
      SELECT  BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-      'ISHOMESITESOLD' AS FIELDNAME, ishomesitesold as FIELDVALUE, 'SALESTATUSIND' ,'W' 
+      'ishomesitesold' AS FIELDANME, ishomesitesold as FIELDVALUE, 'SALESTATUSIND' ,'W' 
      FROM {{ref('SAT_HomeSite')}}
      WHERE ishomesitesold='0' and saledate <>'NULL'
      and cancellationdate='NULL' and ishomesitecancelled='0'         
