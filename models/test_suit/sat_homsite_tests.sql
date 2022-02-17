86ecb24371c9d3212176210e1e0c1f7143895d7b
rishin.pradeep@tredence.com <rishin.pradeep@tredence.com>
2022-02-17 16:57:42 +0000

    chnage

--- models/test_suit/sat_homsite_tests.sql
+++ models/test_suit/sat_homsite_tests.sql
@@ -8,7 +8,7 @@
 {% for column_name in column_names %}
 
        select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE, 
-        '{{column_name}}' AS FIELDANME, {{column_name}} as FIELDVALUE, 
+        '{{column_name}}' AS FIELDNAME, {{column_name}} as FIELDVALUE, 
        '{{column_name}}'||'-DUPLICATE_VALUE' AS ISSUE , 'Error' as ErrorClassification from 
         (select BATCH_ID,MODEL_NAME,{{column_name}}
             FROM {{ref('SAT_HomeSite')}}
@@ -23,7 +23,7 @@
 {% for column_name, desired_len in column_length_check.items() %}
      
        select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-        '{{column_name}}' AS FIELDANME, {{column_name}} as FIELDVALUE,
+        '{{column_name}}' AS FIELDNAME, {{column_name}} as FIELDVALUE,
        '{{column_name}}'||'-LENGTH ISSUE' AS ISSUE , 'Error' as  ErrorClassification 
            FROM {{ref('SAT_HomeSite')}}
            where {{ column_name }} is not null 
@@ -32,24 +32,24 @@
 {% endfor %}
 -- check for valid templateno 
 select BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-'TEMPLATENUMBER' AS FIELDANME, TEMPLATENUMBER as FIELDVALUE, 'INVALID Template NO','W' from {{ref('SAT_HomeSite')}}
+'TEMPLATENUMBER' AS FIELDNAME, TEMPLATENUMBER as FIELDVALUE, 'INVALID Template NO','W' from {{ref('SAT_HomeSite')}}
 where ( TEMPLATENUMBER>190 and TEMPLATENUMBER<200 ) and
 TEMPLATENUMBER <> NULL  
 union all 
  SELECT BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
- 'ACTUALDELIVERYDATE' AS FIELDANME, ACTUALDELIVERYDATE as FIELDVALUE,'DELAY_DELIVERY','W' 
+ 'ACTUALDELIVERYDATE' AS FIELDNAME, ACTUALDELIVERYDATE as FIELDVALUE,'DELAY_DELIVERY','W' 
         FROM {{ref('SAT_HomeSite')}}
         WHERE ADDRESSLINE1 IS NOT NULL AND ADDRESSLINE1<>'NULL'
      AND ACTUALDELIVERYDATE > ESTIMATEDDELIVERYDATE AND SALEDATE<>'NULL' AND ACTUALDELIVERYDATE<>'NULL'
  union all 
     SELECT  BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-     'ishomesitecancelled' AS FIELDANME, ishomesitecancelled as FIELDVALUE, 'CANCEL_STATUS_IND' ,'W' 
+     'ishomesitecancelled' AS FIELDNAME, ishomesitecancelled as FIELDVALUE, 'CANCEL_STATUS_IND' ,'W' 
      FROM {{ref('SAT_HomeSite')}} 
      WHERE ishomesitecancelled='0'
      and cancellationdate <>'NULL'
  union all
      SELECT  BATCH_ID,MODEL_NAME,'HOMESITEID' AS KEYNAME,HOMESITEID AS KEYVALUE,
-      'ishomesitesold' AS FIELDANME, ishomesitesold as FIELDVALUE, 'SALESTATUSIND' ,'W' 
+      'ishomesitesold' AS FIELDNAME, ishomesitesold as FIELDVALUE, 'SALESTATUSIND' ,'W' 
      FROM {{ref('SAT_HomeSite')}}
      WHERE ishomesitesold='0' and saledate <>'NULL'
      and cancellationdate='NULL' and ishomesitecancelled='0'         
